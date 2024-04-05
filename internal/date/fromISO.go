db4742d7a5afd53babaab8ff4c4923634b6b41a7
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2024-04-05 16:26:44 +0300

    Use the sentinel error in the sub package

--- internal/date/fromISO.go
+++ internal/date/fromISO.go
@@ -3,6 +3,8 @@ package date
 import (
 	"fmt"
 	"time"
+
+	"github.com/solrac97gr/searcher/internal/sentinels"
 )
 
 // FromISO8601String takes a date string in ISO8601 format and returns a
@@ -10,7 +12,7 @@ import (
 func (f Formatter) FromISO8601String(s string) (*time.Time, error) {
 	parsedDate, err := time.Parse(time.RFC3339, s)
 	if err != nil {
-		return nil, fmt.Errorf("validation_error: %s", err.Error())
+		return nil, fmt.Errorf("%w: %s", sentinels.ErrValidation, err.Error())
 	}
 
 	parsedDate = parsedDate.UTC()
